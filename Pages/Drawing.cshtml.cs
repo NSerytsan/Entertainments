e5a74534fd2476fb362a48711eee378d1b506abe
NSerytsan <nserytsan@gmail.com>
2022-10-07 10:23:51 +0300

    drawing canvas 1

---
+++ Pages/Drawing.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ButtonGame.Pages
+{
+    public class DrawingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
